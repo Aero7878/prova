b6090b06b347653b2205cb5f13eb492bdb4e782f
Aero7878 <agurioli@yahoo.it>
2025-07-30 01:02:15 +0200

    Add files via upload

---
+++ SRC/App.jsx
@@ -0,0 +1,11 @@
+import VenturiEjectorCalculator from "./components/VenturiEjectorCalculator";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50 py-10">
+      <VenturiEjectorCalculator />
+    </div>
+  );
+}
+
+export default App;
